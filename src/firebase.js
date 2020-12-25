22c577471f5ad662d1443e9e62188fe22b2b7c16
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2020-12-25 17:36:13 +0200

    Header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCqx6znW0iN0OwXXJNbkKdLFjc2g1GIZLU",
+  authDomain: "clone-ab4cb.firebaseapp.com",
+  projectId: "clone-ab4cb",
+  storageBucket: "clone-ab4cb.appspot.com",
+  messagingSenderId: "358175240005",
+  appId: "1:358175240005:web:4b149b688c5feb2109182c",
+  measurementId: "G-2Y4WF6GG19"
+};
