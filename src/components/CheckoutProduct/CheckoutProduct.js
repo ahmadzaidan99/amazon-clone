771dcf7e28aa8d237b9ea01e72ddfb1d7f7123c6
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2021-01-02 03:30:43 +0200

    UI improvments

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "../../StateProvider";
+import StarIcon from "@material-ui/icons/Star";
+import StarHalfIcon from "@material-ui/icons/StarHalf";
+import StarOutlineIcon from "@material-ui/icons/StarBorder";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
+  let halfRating = (rating - Math.floor(rating)) * 10;
+  let outline = 0;
   const removeFromBasket = () => {
     dispatch({
       type: "REMOVE_FROM_BASKET",
@@ -20,11 +25,17 @@ function CheckoutProduct({ id, image, title, price, rating }) {
           <strong>{price}</strong>
         </p>
         <div className="checkoutProduct__rating">
-          {Array(rating)
+          {Array(Math.floor(rating))
             .fill()
-            .map((_, i) => (
-              <p>🌟</p>
+            .map((_, index) => (
+              <StarIcon key={index} />
             ))}
+          {halfRating > 0 ? <StarHalfIcon /> : <></>}
+          {outline > 0
+            ? Array(outline)
+                .fill()
+                .map((_, index) => <StarOutlineIcon key={index} />)
+            : ""}
         </div>
         <button onClick={removeFromBasket}>Remove from basket</button>
       </div>
