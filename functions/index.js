f2763b7205f076b3f9796a76a35670f0b79eff81
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2021-02-10 15:53:34 +0200

    Final Version

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51I5KsoEko8TinVlmKWvhPAmAQLnVSbaNNj0SQoyxw67XalY1vM0GHXYkbTn4tUgSM8BA4U1X3MuX9eKhUMtlFpdr00nBIvYtII"
+);
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (request, response) => response.status(200).send("hello world"));
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+exports.api = functions.https.onRequest(app);
